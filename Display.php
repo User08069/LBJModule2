80e4c26212206982b3304780298d68ac96b1dd44
Jaydeep <jaydeepbhoite2020@gmail.com>
2020-08-20 00:12:57 +0530

    Modified third commit

--- Display.php
+++ Display.php
@@ -1,10 +1,22 @@
-<!DOCTYPE html>
-<html>
 
- <body>
+<?php
+echo "<html><body><table>\n\n";
+$f = fopen("contact_data.csv", "r");
+while (($line = fgetcsv($f)) !== false) {
+        foreach ($line as $cell) {
+            if(is_numeric($cell))
+            {
+                echo "<td>";
+            }
+            else
+            {
+                echo "$cell<br>";
+           
+            }
+        }
+        echo "</td>\n\r";
+}
+fclose($f);
+echo "\n</table></body></html>";
+?>
 
- 
-     
-
-     </body>
-</html>
